9186a279a69df50ddc156ba0483c1a8c8f6d02b3
mkabakcili <mustafa.ihsan.kabakcili@grandcentrix.net>
2024-03-18 20:02:13 +0100

    department controller first part

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -2,3 +2,34 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const DepartmentModel = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(DepartmentModel);
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(DepartmentModel),
+      data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await DepartmentModel.create({ ...req.body });
+
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await DepartmentModel.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {},
+  delete: async (req, res) => {},
+};
--- src/models/department.model.js
+++ src/models/department.model.js
@@ -4,3 +4,25 @@
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+// Declare the Schema of the Mongo model
+var DepartmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    departmentId: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+  },
+  { collection: "Department", timestamps: true }
+);
+
+//Export the model
+module.exports = mongoose.model("Department", DepartmentSchema);
