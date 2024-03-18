ef760a0e1a2189aa408f93a39365e14159f0bbed
mkabakcili <mustafa.ihsan.kabakcili@grandcentrix.net>
2024-03-18 19:35:36 +0100

    start

---
+++ index.js
@@ -0,0 +1,63 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+/*
+    $ npm i express dotenv mongoose express-async-errors
+    $ npm i cookie-session
+    $ npm i jsonwebtoken
+*/
+
+const express = require("express");
+const app = express();
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+//envVariables to process.env.
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
+
+// asyncErrors to errorHandler:
+require("express-async-errors");
+
+/* ------------------------------------------------------- */
+// Configurations:
+
+// Connect to DB:
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json());
+
+// SessionsCookies:
+app.use(require("cookie-session")({ secret: process.env.SECRET_KEY }));
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
+/* ------------------------------------------------------- */
+// Routes:
+
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to Personnel API",
+  });
+});
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require("./src/middlewares/errorHandler"));
+
+// RUN SERVER:
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')()
